b6117757d257f340d33cb224c8ae767f066a6c04
Dmitry Shimansky <Shimansky.Dmitry@gmail.com>
2020-07-13 16:10:11 +0300

    Fixed count

--- lesson18.js
+++ lesson18.js
@@ -101,7 +101,7 @@ window.addEventListener('DOMContentLoaded', () => {
 				return;
 			} else {
 				popUp.style.top = count + 'px';
-				if (count < 550) {
+				if (count < 250) {
 					setTimeout(flyingPopUp, 10);
 				}
 			}
@@ -115,12 +115,48 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 		popUpClose.addEventListener('click', () => {
 			popup.style.display = 'none';
+			popUp.style.top = '0px';
+			count = 0;
 		});
 	};
 
 	togglePopup();
 
+	//Табы
 
+	const tabs = () => {
+		const tabHeader = document.querySelector('.service-header'),
+			tab = tabHeader.querySelectorAll('.service-header-tab'),
+			tabContent = document.querySelectorAll('.service-tab');
+
+		const toggleTabContent = (index) => {
+			for (let i = 0; i < tabContent.length; i++) {
+				if (index === i) {
+					tab[i].classList.add('active');
+					tabContent[i].classList.remove('d-none');
+				} else {
+					tab[i].classList.remove('active');
+					tabContent[i].classList.add('d-none');
+				}
+			}
+		};
+		tabHeader.addEventListener('click', (event) => {
+			let target = event.target;
+
+			while(target !== tabHeader) {
+
+				if (target.classList.contains('service-header-tab')) {
+					tab.forEach((item, i) => {
+						if (item === target) {
+							toggleTabContent(i);
+						}
+					});
+					return;
+				}
+				target = target.parentNode;
+			}
+		});
+	};
 
 
 
